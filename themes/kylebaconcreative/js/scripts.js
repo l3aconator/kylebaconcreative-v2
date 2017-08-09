9c9d04a0c45c2859f79f02d9d7dea06c06f20c16
l3acon <hello@kylebaconcreative.com>
2017-08-08 20:31:52 -0700

    More code clean up and developmemt

--- themes/kylebaconcreative/js/scripts.js
+++ themes/kylebaconcreative/js/scripts.js
@@ -1,23 +1,17 @@
 $(document).on('ready', function(){
-    
+
     //Menu
     $('.js-menu-activate').on('click', function(){
         $('.js-menu').toggleClass('open');
         $('.js-nav-list').slideToggle('hidden');
         $('.js-footer').toggleClass('fadeIn');
     });
-    
-    
+
     //Mobile Menu with filter widget
     $('.js-menu-activate').on('click', function(){
         $('.js-mobile-filter-widget').slideToggle('hidden');
         $('.js-footer').toggleClass('hidden');
     });
-    
-//    Collapse Function
-//    $('.js-collapse-menu').on('click', function(){
-//        $('.docked-nav').add
-//    });
     
     //Smooth Scroll
     $(function() {
@@ -33,37 +27,5 @@ $(document).on('ready', function(){
             }
         }
         });
-    });
-    
-    //Filtering Desktop
-    $('.js-filter-item').on('click', function(){
-        var categoryType = $(this).data('category');
-        $('.js-filter-item').removeClass('active');
-        $(this, '.js-filter-item').addClass('active');
-        $('.work-image-box').fadeOut('slow');
-        $('.' + categoryType).fadeIn('slow');
-    });
-    
-    //Filtering Mobile
-    $('.js-mobile-filter-item').on('click', function(){
-        var categoryType = $(this).data('category');
-        $('.js-mobile-filter-toggle').removeClass('active-mobile');
-        $('.js-mobile-filter-toggle', this).addClass('active-mobile');
-        $('.work-image-box').fadeOut('slow');
-        $('.' + categoryType).fadeIn('slow');
-        
-        //Collapses menu after work category is chosen
-        $('.js-menu').toggleClass('open');
-        $('.js-nav-list').slideToggle('hidden');
-        $('.js-mobile-filter-widget').slideToggle('hidden');
-    }); 
-    
-    
-    //Nesting Anchor Tag Fix
-    $("a").each(function(){
-      var aTag = this;
-      $(aTag).closest('.work-image-box').click(function(){
-          window.location.href = $(aTag).attr('href');
-      });
     });
 });
